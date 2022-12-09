31b9b796e81e144a12761bb1ba7cbb7cbe462914
soojiyou <sooji.you@wsu.edu>
2022-12-08 18:28:25 -0800

    12/08

--- src/client/service-worker.js
+++ src/client/service-worker.js
@@ -1,61 +1,39 @@
-var cacheName = 'geeks-cache-v1';
+const cacheName = "travelcash";
 var cacheAssets = [
     "/",
-    "/index.js",
-    "styles/app.scss",
-    "views/index.html",
-
 ];
 
-// // Call install Event
-// self.addEventListener("install", (event) => {
-//     console.log("Service Worker : Installed!")
 
-//     event.waitUntil(
-
-//         (async () => {
-//             try {
-//                 cache_obj = await caches.open(cache)
-//                 cache_obj.addAll(caching_files)
-//             }
-//             catch {
-//                 console.log("error occured while caching...")
-//             }
-//         })()
-//     )
-// })
-// // Call install Event
-// self.addEventListener('install', e => {
-//     // Wait until promise is finished
-//     e.waitUntil(
-//         caches.open(cacheName)
-//             .then(cache => {
-//                 console.log(`Service Worker: Caching Files: ${cache}`);
-//                 cache.addAll(cacheAssets)
-//                     // When everything is set
-//                     .then(() => self.skipWaiting())
-//             })
-//     );
-// })
+//install
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                console.log(`Service-Worker: Caching`);
+                cache.addAll(cacheAssets)
+                    .then(() => self.skipWaiting())
+            })
+    );
+})
 
 
-// // Call Activate Event
-// self.addEventListener('activate', e => {
-//     console.log('Service Worker: Activated');
+//Activate
+self.addEventListener('activate', event => {
+    console.log('Service-Worker: Activated');
 
-// })
+})
 
 
-// // Call Fetch Event
-// self.addEventListener('fetch', e => {
-//     console.log('Service Worker: Fetching');
-//     e.respondWith(
-//         caches.match(e.request).then(function (response) {
-//             if (response) {
-//                 return response;
-//             }
-//             return fetch(e.request);
-//         })
-//     );
-// });
+// Fetch
+self.addEventListener('fetch', event => {
+    console.log('Service-Worker: Fetching');
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
 
